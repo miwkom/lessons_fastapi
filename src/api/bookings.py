41c3d330b8057be839df6a9f4455fe8168549dc6
miwkom <166976262+miwkom@users.noreply.github.com>
2025-05-14 21:35:02 +0300

    Add files via upload

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -55,3 +55,22 @@ async def create_booking(
     booking = await db.bookings.add(_booking_data)
     await db.commit()
     return {"status": "OK", "data": booking}
+
+
+@router.get("/bookings", summary="Список бронирования")
+async def get_bookings(
+        db: DBDep,
+):
+    return await db.bookings.get_all()
+
+
+@router.get("/bookings/me", summary="Бронирование пользователя")
+async def get_booking(
+        db: DBDep,
+        user_id: UserIdDep,
+):
+    try:
+        user = await db.users.get_one_or_none(id=user_id)
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail="Пользователь не авторизирован")
+    return await db.bookings.get_all(user_id=user.id)
