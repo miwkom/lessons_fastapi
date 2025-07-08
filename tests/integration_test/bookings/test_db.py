3a0f62e3f3dc42d0e7219ddcca9e0490993ad855
miwkom <miwkomxxx@gmail.com>
2025-07-08 08:46:05 +0300

    Исправление ошибок и мелкие изменения

--- tests/integration_test/bookings/test_db.py
+++ tests/integration_test/bookings/test_db.py
@@ -13,37 +13,32 @@ async def test_booking_crud(db):
         date_to=date(year=2026, month=1, day=20),
         price=100,
     )
-    await db.bookings.add(booking_data)
+    new_booking = await db.bookings.add(booking_data)
 
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert booking
+    assert booking.id == new_booking.id
+    assert booking.user_id == new_booking.user_id
+    assert booking.room_id == new_booking.room_id
 
-    booking = await db.bookings.get_one_or_none(user_id=user_id, room_id=room_id)
-
-    assert booking is not None
-    assert booking.date_from == date(year=2025, month=11, day=10)
-    assert booking.date_to == date(year=2026, month=1, day=20)
-    assert booking.price == 100
-
-
+    updated_date_from = date(year=2022, month=4, day=10)
+    updated_date_to = date(year=2026, month=4, day=20)
+    updated_price = 456
     update_booking_data = BookingAdd(
         user_id=user_id,
         room_id=room_id,
-        date_from=date(year=2022, month=4, day=10),
-        date_to=date(year=2024, month=6, day=20),
-        price=456,
+        date_from=updated_date_from,
+        date_to=updated_date_to,
+        price=updated_price,
     )
-    await db.bookings.edit(data=update_booking_data, user_id=user_id, room_id=room_id)
+    await db.bookings.edit(data=update_booking_data, id=new_booking.id)
+    updated_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert updated_booking
+    assert updated_booking.id == new_booking.id
+    assert updated_booking.date_from == updated_date_from
+    assert updated_booking.date_to == updated_date_to
+    assert updated_booking.price == updated_price
 
-    booking = await db.bookings.get_one_or_none(user_id=user_id, room_id=room_id)
-    assert booking is not None
-    assert booking.date_from == date(year=2022, month=4, day=10)
-    assert booking.date_to == date(year=2024, month=6, day=20)
-    assert booking.price == 456
-
-
-    await db.bookings.delete(user_id=user_id, room_id=room_id)
-
-    booking = await db.bookings.get_one_or_none(user_id=user_id, room_id=room_id)
-    assert booking is None
-
-    await db.commit()
-
+    await db.bookings.delete(id=new_booking.id)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert not booking
