ffb187ee76c4fc9fbf436e7a2f6e60e1b63a1b7f
miwkom <miwkomxxx@gmail.com>
2025-07-08 14:04:34 +0300

    Мелкие изменения

--- tests/integration_test/facilities/test_api.py
+++ tests/integration_test/facilities/test_api.py
@@ -2,10 +2,15 @@ async def test_get_facilities(ac):
     response = await ac.get("/facilities")
 
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
 
 
 async def test_create_facilities(ac):
-    facilities_data = {"title": "Wifi"}
-    response = await ac.post("/facilities", json=facilities_data)
-
+    facilities_title = "Wifi"
+    response = await ac.post("/facilities", json={"title": facilities_title})
+    res = response.json()
     assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    assert "data" in res
+    assert res["data"]["title"] == facilities_title
+
